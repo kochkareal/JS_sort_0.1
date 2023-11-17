f89f86fd5a6e8a7b33fedace272621ef6b86a0d9
Никита <118972430+kochkareal@users.noreply.github.com>
2023-11-17 15:19:51 +0300

    refactoring script.js

--- script.js
+++ script.js
@@ -1,71 +1,44 @@
-document.querySelector('#sort-minMax').onclick = SortMinMax; //привязываем кнопки
-document.querySelector('#sort-maxMin').onclick = SortMaxMin;
-document.querySelector('#sort-rait').onclick = SortMaxMinRait; 
-document.querySelector('#add-item').onclick = AddRec; 
+document.querySelector('#sort-minMax').onclick = () => sortItems('tovar-price', 'asc'); 
+document.querySelector('#sort-maxMin').onclick = () => sortItems('tovar-price', 'desc');
+document.querySelector('#sort-rait').onclick = () => sortItems('raiting', 'desc'); 
+document.querySelector('#add-item').onclick = () => 
+    AddItem(document.getElementById("title").value, 
+            document.getElementById("price").value, 
+            document.getElementById("raiting").value); 
 document.querySelector('#add-random').onclick = AddRandom; 
 
-function SortMinMax(){                                       //от меньшего к большему
+function sortItems(sortBy, order){
     let tovar = document.querySelector('#tovar');
-    for (let i = 0; i < tovar.children.length; i++){
-        for (let j = i; j < tovar.children.length; j++) {
-            if(+tovar.children[i].getAttribute('tovar-price')> +tovar.children[j].getAttribute('tovar-price')){
-                replacedNode = tovar.replaceChild(tovar.children[j], tovar.children[i]);
-                insertAfter(replacedNode, tovar.children[i]);
-            }
-        }
-    }
-}
 
-function SortMaxMin(){                                          //от большего к меньшему
-    let tovar = document.querySelector('#tovar');
-    for (let i = 0; i < tovar.children.length; i++){
-        for (let j = i; j < tovar.children.length; j++) {
-            if(+tovar.children[i].getAttribute('tovar-price')< +tovar.children[j].getAttribute('tovar-price')){
-                replacedNode = tovar.replaceChild(tovar.children[j], tovar.children[i]);
-                insertAfter(replacedNode, tovar.children[i]);
-            }
+    Array.from(tovar.children)
+      .sort((a, b) => {
+        let aVal = +a.getAttribute(sortBy);
+        let bVal = +b.getAttribute(sortBy);
+        if(order == 'asc') {
+          return aVal - bVal;
         }
-    }
-}
-
-function SortMaxMinRait(){                                          //от большего к меньшему
-    let tovar = document.querySelector('#tovar');
-    for (let i = 0; i < tovar.children.length; i++){
-        for (let j = i; j < tovar.children.length; j++) {
-            if(+tovar.children[i].getAttribute('raiting')< +tovar.children[j].getAttribute('raiting')){
-                replacedNode = tovar.replaceChild(tovar.children[j], tovar.children[i]);
-                insertAfter(replacedNode, tovar.children[i]);
-            }
+        else {
+          return bVal - aVal;
         }
-    }
-}
-
-function insertAfter(elem, refElem){
-    return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
+      })
+      .forEach((child) => {
+         tovar.appendChild(child);
+      });
 }
 
 function AddItem(title, price, raiting){
     var newDiv = document.createElement("div");
-
-    // Задаем класс элемента
     newDiv.classList.add("tovar-item");
-
-    // Устанавливаем атрибуты элемента
     newDiv.setAttribute("raiting", raiting);
     newDiv.setAttribute("tovar-price", price);
-    
-    newDiv.textContent = title + " " + price + "$ (" + raiting + ")";
+    newDiv.textContent = `${title} ${price}$ (${raiting})`;
     document.getElementById("tovar").appendChild(newDiv);
 }
 
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return (Math.random() * (max - min + 1)) + min;
 }
 
 function AddRandom(){
-   AddItem("Случайный товар", getRandomNumber(1, 9999), getRandomNumber(1, 10)) ;
-}
-
-function AddRec(){
-AddItem(document.getElementById("title").value, document.getElementById("price").value, document.getElementById("raiting").value)
+   AddItem("Случайный товар", getRandomNumber(1, 9999), getRandomNumber(1, 10));
 }
